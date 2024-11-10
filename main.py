e96453284cdbaaf2ce63625285cf6c21224f7c7d
firasaj <firas.aj@icloud.com>
2024-11-09 23:01:52 -0500

    further fixed main.py, update to v.01 coming soon

--- main.py
+++ main.py
@@ -34,9 +34,9 @@ def main():
         merge_parser.add_argument("sp_playlist_name", help="Name of the Spotify playlist to merge.")
 
         # Download YT song
-        yt_to_sp_parser = subparsers.add_parser("download_yt_song", help="Download YouTube song to MP3.")
-        yt_to_sp_parser.add_argument("song_name", help="Name of the Youtube song to download.")
-        yt_to_sp_parser.add_argument("artists", help="Name of the artists associated with the song.")
+        download_parser = subparsers.add_parser("download_yt_song", help="Download YouTube song to MP3.")
+        download_parser.add_argument("song_name", help="Name of the Youtube song to download.")
+        download_parser.add_argument("artists", help="Name of the artists associated with the song.")
 
         # Add "exit" command
         parser.add_argument("exit", nargs='?', help="Type 'exit' to quit the app.")
@@ -55,9 +55,13 @@ def main():
             print("Merging Spotify and Youtube playlists...")
             merge_playlists(args.yt_playlist_name, args.sp_playlist_name)
         
-        elif args.command == "merge":
+        elif args.command == "download_yt_song":
             print("Downloading YouTube song...")
             download_yt_song(args.song_name, args.artists)
+
+        elif args.command == 'help' or '-h' in sys.argv or '--help' in sys.argv:
+            parser.print_help()  # Show help message
+            continue  # Stay in the loop
 
         elif args.exit == "exit":
             print("Exiting the app. Goodbye!")
@@ -67,10 +71,10 @@ def main():
             print("Welcome to the Syncer App!")
             print("Type out your command below and press Enter:")
             print("Available commands:")
-            print("  sync_yt_to_sp <yt_playlist_name> - Sync Youtube playlist to Spotify")
-            print("  sync_sp_to_yt <sp_playlist_name> - Sync Spotify playlist to Youtube")
-            print("  merge <yt_playlist_name> <sp_playlist_name> - Merge Spotify and Youtube playlists")
-            print("  download_yt_song <song_name> <artists> - Download YouTube song to MP3")
+            print("  sync_yt_to_sp \"<yt_playlist_name>\" - Sync Youtube playlist to Spotify")
+            print("  sync_sp_to_yt \"<sp_playlist_name>\" - Sync Spotify playlist to Youtube")
+            print("  merge \"<yt_playlist_name>\" \"<sp_playlist_name>\" - Merge Spotify and Youtube playlists")
+            print("  download_yt_song \"<song_name>\" \"<artists>\" - Download YouTube song to MP3")
             print("  exit - Quit the app")
             
             # Prompt for user input if no arguments are provided
@@ -88,6 +92,7 @@ if __name__ == "__main__":
     except Exception as e:
         print(f"An error occurred: {e}")
         input("Press Enter to exit...")
+
 
 
 # COMMAND TO BUILD: pyinstaller --onefile --add-data "src;src" --add-data ".env;." main.py
